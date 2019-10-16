9db3a815adc4be40221ec2570282f46470a5e620
Kentarou Ueda <ueken0307@hotmail.co.jp>
2019-10-16 10:47:57 +0900

    update コンストラクタの初期化ミス修正

--- include/RhythmGameManager.cpp
+++ include/RhythmGameManager.cpp
@@ -8,7 +8,7 @@ NoteData::NoteData(int count,double second,int lane,int length,bool isEndEffect)
   this->isEndEffect = isEndEffect;
 }
 
-RhythmGameManager::RhythmGameManager(std::vector<NoteData> notes, std::vector<BpmData> bpms, double offset, bool isAuto):rhythmManager(bpms,offset), isAuto(false){
+RhythmGameManager::RhythmGameManager(std::vector<NoteData> notes, std::vector<BpmData> bpms, double offset, bool isAuto):rhythmManager(bpms,offset), isAuto(isAuto){
   for (auto i : notes) {
     this->notes.push_back(i);
   }
